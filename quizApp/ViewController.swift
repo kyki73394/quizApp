4e1840707b914a4f1f0fe2951143185494e67d5e
Hannah <kyki73394@gmail.com>
2020-07-28 10:55:09 -0400

    Fixed spelling in last view controller.

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Apple on 7/23/20.
+//  Copyright © 2020 Hannah Kim. All rights reserved.
+//
+
+//Images from Unsplash by
+//leonides ruvalcabar, Kourosh Qaffari, Danielle Dolson, Patrick Assalé, Diego González, <span>Photo by <a href="https://unsplash.com/@tabithabrooke?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">tabitha turner</a> on <a href="https://unsplash.com/s/photos/daisies?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>and Cody Board
+//Images on "Pick another artist" page from following sources
+//Taylor Swift https://i.ytimg.com/vi/cMPEd8m79Hw/maxresdefault.jpg
+//TWICE https://i.scdn.co/image/2329c39c9c2fe817cdaa89dfca2ce857793cb0a0
+//Sara Bareilles https://lastfm.freetls.fastly.net/i/u/770x0/86e7b98acf664403a3e55d3a6c9bf227.jpg
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
